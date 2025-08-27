1f09fd13e7d789e59c55ee9933e099e857bf67e4
dhkwak <dreamaboutme.k@gmail.com>
2025-08-27 17:56:53 +0900

    connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+  res.send("Hello world");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB와 연결 되었습니다."))
+  .catch((err) => console.log("Mongodb와 연결 실패", err));
 
 app.listen(PORT, () => {
-    console.log("Server is running");
-})
+  console.log("Server is running");
+});
